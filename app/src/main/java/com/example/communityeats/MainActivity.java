dc19e4d7f42559087ec44c71e796b230eb8e79d4
Thomas Lao <laothomas01@gmail.com>
2021-09-27 21:26:21 -0700

    working on register button
    working on register button

--- app/src/main/java/com/example/communityeats/MainActivity.java
+++ app/src/main/java/com/example/communityeats/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.communityeats;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+        private Button register;
+        private TextView nameRegister;
+        private TextView passwordRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (Button) findViewById(R.id.btnRegister);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
