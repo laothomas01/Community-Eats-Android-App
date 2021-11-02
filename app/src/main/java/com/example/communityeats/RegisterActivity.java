fb8a008f62d7f2a0567000ccd0c4252eb564a8be
Andrew Yu <yingzheng.yu@sjsu.edu>
2021-11-01 23:22:03 -0700

    Pair programming with Thomas L. and Jahnae R.

--- app/src/main/java/com/example/communityeats/RegisterActivity.java
+++ app/src/main/java/com/example/communityeats/RegisterActivity.java
@@ -19,7 +19,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 
 public class RegisterActivity extends Activity implements View.OnClickListener {
-    //TEST
+    //Pair programming with Thomas L. via Android Studio tool feature
     private FirebaseAuth mAuth;
     private Button register;
     private Button haveAccount;
@@ -30,7 +30,7 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
           super.onCreate(savedInstanceState);
           setContentView(R.layout.activity_register);
           mAuth = FirebaseAuth.getInstance();
-          //TEST...
+          //Pair programming with Thomas L. via Android Studio tool feature
           register = (Button) findViewById(R.id.btnRegister);
           register.setOnClickListener(this);
           haveAccount = (Button) findViewById(R.id.btnHaveAccount);
