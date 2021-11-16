142ff1d70e078bba0d2347c12879dd5fef1db44e
jreese14 <jreesestige@gmail.com>
2021-11-15 23:21:19 -0800

    Homescreen marketplace
    Added homescreen

--- app/src/main/java/com/example/communityeats/FoodDonationItem.java
+++ app/src/main/java/com/example/communityeats/FoodDonationItem.java
@@ -1,14 +1,14 @@
 package com.example.communityeats;
 
 public class FoodDonationItem {
-    public String date, foodName,foodQuantity,foodDescription,foodImageUrl;
+    public String date, foodName,foodQuantity,foodDescription,foodImageUrl, donorID, recipientID;
 
     public FoodDonationItem() {
 
     }
 
     public FoodDonationItem(String date, String foodName, String foodQuantity,
-                            String foodDescription, String foodImageUrl)
+                            String foodDescription, String foodImageUrl, String donorID, String recipientID)
     {
 
         this.date = date;
@@ -16,8 +16,33 @@ public class FoodDonationItem {
         this.foodQuantity = foodQuantity;
         this.foodDescription = foodDescription;
         this.foodImageUrl = foodImageUrl;
+        this.donorID = donorID;
+        this.recipientID = recipientID;
 
 
     }
 
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getFoodQuantity() {
+        return foodQuantity;
+    }
+
+    public void setFoodQuantity(String foodQuantity) {
+        this.foodQuantity = foodQuantity;
+    }
+
+    public String getFoodDescription() {
+        return foodDescription;
+    }
+
+    public void setFoodDescription(String foodDescription) {
+        this.foodDescription = foodDescription;
+    }
 }
--- app/src/main/java/com/example/communityeats/HomeScreenActivity.java
+++ app/src/main/java/com/example/communityeats/HomeScreenActivity.java
@@ -3,7 +3,10 @@ package com.example.communityeats;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,17 +14,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class HomeScreenActivity extends AppCompatActivity {
-
+    private DatabaseReference FoodItemsRef;
+    private RecyclerView recyclerView;
+    MyAdapter myAdapter;
+    ArrayList<FoodDonationItem> list;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+
+        FoodItemsRef = FirebaseDatabase.getInstance().getReference("FoodDonationItem");
+
+        recyclerView = findViewById(R.id.market_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        myAdapter = new MyAdapter(this, list);
+        recyclerView.setAdapter(myAdapter);
+
+        FoodItemsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    FoodDonationItem foodItem = dataSnapshot.getValue(FoodDonationItem.class);
+                    list.add(foodItem);
+                }
+                myAdapter.notifyDataSetChanged();
+                System.out.println("Food Donation Item Added");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
@@ -49,9 +95,6 @@ public class HomeScreenActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
     }
+
 }
